f077fe9fc7bfa5c30cfd4f1a89758321aa293a5d
Vandilyn <exrenaz112@gmail.com>
2022-12-18 22:56:46 -0800

    post

---
+++ newTeddit/PostList/PostList+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PostList+CoreDataClass.swift
+//  newTeddit
+//
+//  Created by prk on 18/12/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PostList)
+public class PostList: NSManagedObject {
+
+}
---
+++ newTeddit/Posts/Posts+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Posts+CoreDataClass.swift
+//  newTeddit
+//
+//  Created by prk on 18/12/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Posts)
+public class Posts: NSManagedObject {
+
+}
